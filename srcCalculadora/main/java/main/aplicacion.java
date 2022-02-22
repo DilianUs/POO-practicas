404e74663baf5919344e5731a80b7efffb5ee0e2
DilianUs <75262812+DilianUs@users.noreply.github.com>
2022-02-21 19:01:39 -0600

    Add files via upload

---
+++ srcCalculadora/main/java/main/aplicacion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import vista.CalculadoraP;
+
+/**
+ *
+ * @author braul
+ */
+public class aplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CalculadoraP ven = new CalculadoraP();
+        
+        ven.setVisible(true);
+        
+    }
+    
+}
